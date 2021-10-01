e7caabe27de0e88a7900008a024d3efc8901886a
stkirk <shaanekirk@gmail.com>
2021-10-01 14:02:00 -0400

    installed dependencies, server running on port 9000 with catchall endpoint

--- api/server.js
+++ api/server.js
@@ -1,2 +1,13 @@
 // implement your server here
+const express = require("express");
+const server = express();
+server.use(express.json());
+
 // require your posts router and connect it here
+
+//Catchall endpoint
+server.use("*", (req, res) => {
+  res.status(404).json({ message: "Error 404: Not Found" });
+});
+
+module.exports = server;
