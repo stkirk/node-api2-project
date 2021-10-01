46003f39fc35eb719f0c9e5951d5a2b35ccebd14
stkirk <shaanekirk@gmail.com>
2021-10-01 15:10:04 -0400

    put endpoint working to spec

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -59,4 +59,31 @@ router.post("/", (req, res) => {
   }
 });
 
+router.put("/:id", (req, res) => {
+  const changes = req.body;
+  const { id } = req.params;
+
+  if (!changes.title || !changes.contents) {
+    res
+      .status(400)
+      .json({ message: "Please provide title and contents for the post" });
+  } else {
+    Post.update(id, changes)
+      .then((updatedPost) => {
+        if (updatedPost === 1) {
+          res.status(200).json({ ...changes, id: parseInt(id) });
+        } else {
+          res
+            .status(404)
+            .json({ message: "The post with the specified ID does not exist" });
+        }
+      })
+      .catch(() => {
+        res
+          .status(500)
+          .json({ message: "The post information could not be modified" });
+      });
+  }
+});
+
 module.exports = router;
