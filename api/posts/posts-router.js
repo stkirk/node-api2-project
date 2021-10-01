d3902ab4907951e89802601bd7f56c4b4bc14bfb
stkirk <shaanekirk@gmail.com>
2021-10-01 14:45:22 -0400

    post endpoint working to spec

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,10 +1,62 @@
 // implement your posts router here
 const express = require("express");
 
+const Post = require("./posts-model.js");
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "get posts endpoint working" });
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.status(200).json(posts);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "The posts information could not be retrieved" });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const post = await Post.findById(id);
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist" });
+    }
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "The post information could not be retrieved" });
+  }
+});
+
+router.post("/", (req, res) => {
+  const post = req.body;
+  console.log("NEW POST--> ", req.body);
+
+  if (!post.title || !post.contents) {
+    res
+      .status(400)
+      .json({ message: "Please provide title and contents for the post" });
+  } else {
+    Post.insert(post)
+      .then((posted) => {
+        res.status(201).json({
+          ...posted,
+          title: post.title,
+          contents: post.contents,
+        });
+      })
+      .catch(() => {
+        res.status(500).json({
+          message: "There was an error while saving the post to the database",
+        });
+      });
+  }
 });
 
 module.exports = router;
